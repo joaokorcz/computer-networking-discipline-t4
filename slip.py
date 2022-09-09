1d45f82e90eed80d61cf4fb60b862d8a9a55985e
João Otavio <joaokorcz@hotmail.com>
2022-09-09 11:39:07 -0300

    implemented Enlace methods (exercises)

--- slip.py
+++ slip.py
@@ -43,6 +43,8 @@ class Enlace:
     def __init__(self, linha_serial):
         self.linha_serial = linha_serial
         self.linha_serial.registrar_recebedor(self.__raw_recv)
+        self.fernandola = b'' # dados atuais
+        self.sinal = False
 
     def registrar_recebedor(self, callback):
         self.callback = callback
@@ -51,14 +53,53 @@ class Enlace:
         # TODO: Preencha aqui com o código para enviar o datagrama pela linha
         # serial, fazendo corretamente a delimitação de quadros e o escape de
         # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        pass
+        data = b''
+
+        for byte in datagrama:
+            if byte.to_bytes(1, 'big') == b'\xC0':
+                data += b'\xDB\xDC'
+            elif byte.to_bytes(1, 'big') == b'\xDB':
+                data += b'\xDB\xDD'
+            else:
+                data += byte.to_bytes(1, 'big')
+                
+        data = b'\xC0' + data + b'\xC0'
+        self.linha_serial.enviar(data)
 
     def __raw_recv(self, dados):
         # TODO: Preencha aqui com o código para receber dados da linha serial.
         # Trate corretamente as sequências de escape. Quando ler um quadro
         # completo, repasse o datagrama contido nesse quadro para a camada
         # superior chamando self.callback. Cuidado pois o argumento dados pode
-        # vir quebrado de várias formas diferentes - por exemplo, podem vir
+        # vir quebrado de várias formas difNoneerentes - por exemplo, podem vir
         # apenas pedaços de um quadro, ou um pedaço de quadro seguido de um
         # pedaço de outro, ou vários quadros de uma vez só.
-        pass
+
+        ferdinandinho = self.fernandola # conteudo recebido AGORA
+        
+        for byte in dados:
+            if byte.to_bytes(1, 'big') == b'\xC0':
+                if len(ferdinandinho) > 0:
+                    try:
+                        self.callback(ferdinandinho)
+                    except:
+                        import traceback
+                        traceback.print_exc()
+                    finally:
+                        ferdinandinho = b''
+                    
+            elif byte.to_bytes(1, 'big') == b'\xDB':
+                self.sinal = True
+
+            elif self.sinal and byte.to_bytes(1, 'big') == b'\xDC':
+                self.sinal = False
+                ferdinandinho += b'\xC0'
+
+            elif self.sinal and byte.to_bytes(1, 'big') == b'\xDD':
+                self.sinal = False
+                ferdinandinho += b'\xDB'
+
+            else:
+                ferdinandinho += byte.to_bytes(1, 'big')
+
+        self.fernandola = ferdinandinho
